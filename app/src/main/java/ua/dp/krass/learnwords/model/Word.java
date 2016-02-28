37d002997b1dd8d26ec09bb826d8f65ce4ace291
Krass <fanfm2008@gmail.com>
2016-02-29 00:01:23 +0200

    commit

---
+++ app/src/main/java/ua/dp/krass/learnwords/model/Word.java
@@ -0,0 +1,11 @@
+package ua.dp.krass.learnwords.model;
+
+/**
+ * Created by Krass Andrey on 28.02.2016.
+ */
+public class Word {
+    private String engName;
+    private String rusName;
+    private boolean engDone;
+    private boolean rusDone;
+}
