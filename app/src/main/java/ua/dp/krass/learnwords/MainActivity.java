e4a94b649e3ed3cc64c5ceae681c4cdd688a4683
Krass <fanfm2008@gmail.com>
2016-02-28 11:39:05 +0200

    add project

---
+++ app/src/main/java/ua/dp/krass/learnwords/MainActivity.java
@@ -0,0 +1,21 @@
+package ua.dp.krass.learnwords;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.HashMap;
+
+public class MainActivity extends AppCompatActivity {
+
+     HashMap<String, String> words;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        
+
+        words = new HashMap<>();
+    }
+}
